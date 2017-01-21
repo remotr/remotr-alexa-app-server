8aa91cec04649af939f5fd10897a4702681d8f94
Soren <dev@remoteconsult.dk>
2017-01-21 15:08:37 +0100

    A

--- examples/server.js
+++ examples/server.js
@@ -1,8 +1,8 @@
 var AlexaAppServer = require("../index.js");
 AlexaAppServer.start({
 		//port:8080,
-		app_dir: "./examples/apps",
-		server_dir : "./examples/server",
+		app_dir: "apps",
+		server_dir : "server",
 		debug : true,
     // By default, some information is logged with console.log(), which can be disabled
     log : true,

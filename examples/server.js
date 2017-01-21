c0cef688877836b144ce9b8904b221ab260040ea
Soren <dev@remoteconsult.dk>
2017-01-21 15:12:07 +0100

    A

--- examples/server.js
+++ examples/server.js
@@ -14,7 +14,7 @@ AlexaAppServer.start({
     public_html:"public_html", // Static content
 //    app_dir:"apps",            // Where alexa-app modules are stored
     app_root:"/alexa/",        // Service root
-    port:8080
+    port:process.env.PORT || 8080
 	// Use preRequest to load user data on each request and add it to the request json.
 	// In reality, this data would come from a db or files, etc.
 	// 	preRequest: function(json,req,res) {

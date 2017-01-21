6d34054fa599c48897f139b1f28ce3a920bf3f45
Soren <dev@remoteconsult.dk>
2017-01-21 14:50:02 +0100

    A

--- examples/server.js
+++ examples/server.js
@@ -1,7 +1,7 @@
 var AlexaAppServer = require("../index.js");
 AlexaAppServer.start({
-		port:8080,
-		app_dir: "apps",
+		port:80,
+		app_dir: "./examples/apps",
 		server_dir : "./examples/server",
 		debug : true,
     // By default, some information is logged with console.log(), which can be disabled
@@ -9,16 +9,16 @@ AlexaAppServer.start({
     // This will add verification for alexa requests as require by the alexa certification
     // process. Provied by alexa-verifier
     verify: false,
-		httpsEnabled : false,
+		httpsEnabled : false
 
 	// Use preRequest to load user data on each request and add it to the request json.
 	// In reality, this data would come from a db or files, etc.
-		preRequest: function(json,req,res) {
-				console.log("preRequest fired");
-				json.userDetails = { "name":"Bob Smith" };
-		},
-	// Add a dummy attribute to the response
-		postRequest: function(json,req,res) {
-				json.dummy = "text";
-		}
+	// 	preRequest: function(json,req,res) {
+	// 			console.log("preRequest fired");
+	// 			json.userDetails = { "name":"Bob Smith" };
+	// 	},
+	// // Add a dummy attribute to the response
+	// 	postRequest: function(json,req,res) {
+	// 			json.dummy = "text";
+	// 	}
 });
